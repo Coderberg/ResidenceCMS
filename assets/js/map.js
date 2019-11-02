1d26b94f71cfcbcd074ff93ea1ae8e1d4d73bc14
Valery Maslov <vmaslov@live.ru>
2019-11-02 11:06:16 +0300

    Minor design improvement

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,23 @@
 /** global: ymaps */
-ymaps.ready(function () {
-    var t = new ymaps.Map("map", {center: [latitude, longitude], zoom: 13});
-    cur_flat = new ymaps.Placemark([latitude, longitude], {
+ymaps.ready(init);
+function init(){
+    // Creating the map.
+    let myMap = new ymaps.Map("map", {
+        center: [latitude, longitude],
+        zoom: 13
+    });
+
+    let currentApartment = new ymaps.Placemark([latitude, longitude], {
         hintContent: hintContent,
         balloonContent: balloonContent
-    }, {preset: "islands#blueHomeIcon"}), t.geoObjects.add(cur_flat)
-});
+    }, {
+        preset: "islands#blueHomeIcon"
+    });
 
-$("#map").css({width: "100%", height: "250px", "padding-top": "20px"});
+    myMap.geoObjects.add(currentApartment)
+}
+
+$("#map").css({
+    width: "100%",
+    height: "280px", "padding-top": "20px"
+});
