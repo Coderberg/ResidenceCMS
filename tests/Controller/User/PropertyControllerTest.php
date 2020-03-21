19f044bebd93af1d18670747dde5a2bc8fa8c36e
Valery Maslov <vmaslov@live.ru>
2020-03-21 09:44:29 +0300

    Added missing tests

--- tests/Controller/User/PropertyControllerTest.php
+++ tests/Controller/User/PropertyControllerTest.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller\User;
 
+use App\Entity\Property;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class PropertyControllerTest extends WebTestCase
 {
+    private const USER = [
+        'PHP_AUTH_USER' => 'user',
+        'PHP_AUTH_PW' => 'user',
+    ];
+
     public function testIndex()
     {
-        $client = static::createClient([], [
-            'PHP_AUTH_USER' => 'user',
-            'PHP_AUTH_PW' => 'user',
-        ]);
+        $client = static::createClient([], self::USER);
 
         $crawler = $client->request('GET', '/user/property');
         $this->assertResponseIsSuccessful(sprintf('The %s public URL loads correctly.', '/user/account'));
@@ -22,5 +26,70 @@ final class PropertyControllerTest extends WebTestCase
 
         $client->request('GET', '/admin');
         $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testUnpublish()
+    {
+        $client = static::createClient([], self::USER);
+
+        $user = $client->getContainer()->get('doctrine')
+            ->getRepository(User::class)
+            ->findOneBy(['username' => 'user']);
+
+        $property = $client->getContainer()->get('doctrine')
+            ->getRepository(Property::class)
+            ->findOneBy(['author' => $user]);
+
+        $client->request('GET', sprintf('/user/property/%d/unpublish', $property->getId()));
+
+        // asserts that the "Content-Type" header is "application/json"
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+
+        $this->assertContains('ok', $client->getResponse()->getContent());
+    }
+
+    public function testUnpublishForbidden()
+    {
+        $client = static::createClient([], self::USER);
+
+        $user = $client->getContainer()->get('doctrine')
+            ->getRepository(User::class)
+            ->findOneBy(['username' => 'admin']);
+
+        $property = $client->getContainer()->get('doctrine')
+            ->getRepository(Property::class)
+            ->findOneBy(['author' => $user]);
+
+        $client->request('GET', sprintf('/user/property/%d/unpublish', $property->getId()));
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testPublish()
+    {
+        $client = static::createClient([], self::USER);
+
+        $property = $client->getContainer()->get('doctrine')
+            ->getRepository(Property::class)
+            ->findOneBy(['state' => 'private']);
+
+        $client->request('GET', sprintf('/user/property/%d/publish', $property->getId()));
+
+        // asserts that the "Content-Type" header is "application/json"
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+
+        $this->assertContains('ok', $client->getResponse()->getContent());
     }
 }
