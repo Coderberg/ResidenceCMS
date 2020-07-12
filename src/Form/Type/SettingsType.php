1dcaf4e396acd079dd7af464015d35f44ec9a935
Valery Maslov <vmaslov@live.ru>
2020-07-12 12:57:01 +0300

    Minor refactoring

--- src/Form/Type/SettingsType.php
+++ src/Form/Type/SettingsType.php
@@ -25,68 +25,43 @@ final class SettingsType extends AbstractType
             ->add('name', null, [
                 'attr' => [
                     'autofocus' => true,
-                    'class' => 'form-control',
                 ],
                 'label' => 'label.settings.name',
                 'constraints' => [new Length(['min' => 2])],
             ])
             ->add('title', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.settings.title',
                 'constraints' => [new Length(['min' => 4])],
             ])
             ->add('meta_title', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.meta_title',
                 'constraints' => [new Length(['min' => 8])],
             ])
             ->add('meta_description', TextareaType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.meta_description',
                 'constraints' => [new Length(['min' => 8])],
             ])
             ->add('custom_code', TextareaType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'required' => false,
                 'label' => 'label.settings.code',
             ])
             ->add('custom_footer_text', TextareaType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'required' => false,
                 'label' => 'label.settings.custom_footer_text',
             ])
             ->add('items_per_page', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.settings.limit',
                 'constraints' => [new Range(['min' => 3, 'max' => 100])],
             ])
             ->add('currency', EntityType::class, [
                 'class' => Currency::class,
                 'choice_label' => 'currency_title',
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.settings.currency',
             ])
             ->add('fixed_top_navbar', ChoiceType::class, [
                     'choices' => [
                         'option.off' => '0',
                         'option.on' => '1',
-                    ],
-                    'attr' => [
-                        'class' => 'form-control',
                     ],
                     'label' => 'label.settings.fixed_top_navbar',
                 ]
@@ -96,31 +71,21 @@ final class SettingsType extends AbstractType
                         'option.off' => '0',
                         'option.on' => '1',
                     ],
-                    'attr' => [
-                        'class' => 'form-control',
-                    ],
                     'label' => 'label.settings.show_similar_properties',
                 ]
             )
             ->add('ymaps_key', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'required' => false,
                 'label' => 'label.settings.ymaps_key',
             ])
             ->add('map_center', null, [
                 'attr' => [
-                    'class' => 'form-control',
                     'placeholder' => 'placeholder.map_center_example',
                 ],
                 'required' => false,
                 'label' => 'label.settings.map_center',
             ])
             ->add('map_zoom', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
                 'label' => 'label.settings.map_zoom',
                 'constraints' => [new Range(['min' => 0, 'max' => 19])],
             ]);
