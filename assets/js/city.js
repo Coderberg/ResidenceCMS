36d6146d2db26e2f272dd32a1b177766b97efeb8
Plamen Nikolov <plamen.nikolov89@gmail.com>
2021-01-03 12:29:04 +0200

    Add registration to the system Make some changes for good writing and conversion from Bulgarian to English in Transliterator.php Fix margin-top of body.body

--- assets/js/city.js
+++ assets/js/city.js
@@ -19,9 +19,6 @@ $(document).ready(function () {
             type: $form.attr('method'),
             data: data,
             success: function (html) {
-                $('#property_district').replaceWith(
-                    $(html).find('#property_district')
-                );
                 $('#property_neighborhood').replaceWith(
                     $(html).find('#property_neighborhood')
                 );
