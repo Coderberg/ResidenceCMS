b60cd9b94593189babc993923a4de618b4747dfb
Plamen Nikolov <plamen.nikolov89@gmail.com>
2021-01-07 01:06:38 +0200

    Remove empty line before closing curly brace of Class

--- src/DataFixtures/PageFixtures.php
+++ src/DataFixtures/PageFixtures.php
@@ -103,5 +103,4 @@ final class PageFixtures extends Fixture
                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>';
     }
-
 }
