4b80080aaac5c3c354fef533ead6bd54bf1e82ee
Valery Maslov <vmaslov@live.ru>
2021-06-06 13:43:56 +0300

    minor

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,8 +8,8 @@ use App\Entity\Traits\EntityIdTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
