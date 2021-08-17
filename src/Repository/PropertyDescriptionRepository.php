ba72ae6e6791fa77db5f3e57d5c13da20f4119e7
Valery Maslov <vmaslov@live.ru>
2021-08-17 22:56:13 +0300

    Minor

--- src/Repository/PropertyDescriptionRepository.php
+++ src/Repository/PropertyDescriptionRepository.php
@@ -14,39 +14,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method PropertyDescription[]    findAll()
  * @method PropertyDescription[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class PropertyDescriptionRepository extends ServiceEntityRepository
+final class PropertyDescriptionRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PropertyDescription::class);
     }
-
-    // /**
-    //  * @return PropertyDescription[] Returns an array of PropertyDescription objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?PropertyDescription
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
