e4404f3a4884825534660c64442249c30ff57066
Valery Maslov <vmaslov@live.ru>
2022-01-11 19:30:10 +0300

    Code refactoring

--- src/Controller/User/Ajax/PhotoController.php
+++ src/Controller/User/Ajax/PhotoController.php
@@ -4,19 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller\User\Ajax;
 
+use App\Controller\AbstractPhotoController;
 use App\Controller\AjaxController;
-use App\Controller\BaseAjaxController;
-use App\Entity\Photo;
 use App\Entity\Property;
 use App\Service\FileUploader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\ConstraintViolation;
 
-final class PhotoController extends BaseAjaxController implements AjaxController
+final class PhotoController extends AbstractPhotoController implements AjaxController
 {
     /**
      * @Route("/user/photo/{id<\d+>}/upload", name="user_photo_upload", methods={"POST"})
@@ -24,30 +21,7 @@ final class PhotoController extends BaseAjaxController implements AjaxController
      */
     public function upload(Property $property, Request $request, FileUploader $fileUploader): JsonResponse
     {
-        /** @var UploadedFile $uploadedFile */
-        $uploadedFile = $request->files->get('file');
-        $violations = $fileUploader->validate($uploadedFile);
-
-        if ($violations->count() > 0) {
-            /** @var ConstraintViolation $violation */
-            $violation = $violations[0];
-            $this->addFlash('danger', $violation->getMessage());
-
-            return new JsonResponse(['status' => 'fail'], 422);
-        }
-
-        $fileName = $fileUploader->upload($uploadedFile);
-
-        $photo = new Photo();
-        $photo->setProperty($property)
-                ->setSortOrder(0)
-                ->setPhoto($fileName);
-
-        $em = $this->doctrine->getManager();
-        $em->persist($photo);
-        $em->flush();
-
-        return new JsonResponse(['status' => 'ok']);
+        return $this->uploadPhoto($property, $request, $fileUploader);
     }
 
     /**
@@ -58,10 +32,6 @@ final class PhotoController extends BaseAjaxController implements AjaxController
      */
     public function sort(Request $request, Property $property): JsonResponse
     {
-        $ids = $request->request->all('ids');
-        $repository = $this->doctrine->getRepository(Photo::class);
-        $repository->reorderPhotos($property, $ids);
-
-        return new JsonResponse(['status' => 'ok']);
+        return $this->sortPhotos($request, $property);
     }
 }
