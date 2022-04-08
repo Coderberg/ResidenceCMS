63c1f3fa08cbe38cc4e0da8d1ccffd1c0a5f5eff
Valery Maslov <vmaslov@live.ru>
2022-04-08 09:34:37 +0300

    Update AuthTest

--- tests/E2E/AuthTest.php
+++ tests/E2E/AuthTest.php
@@ -8,6 +8,21 @@ use Symfony\Component\Panther\PantherTestCase;
 
 final class AuthTest extends PantherTestCase
 {
+    public function testLoginAsUserWithWrongPassword(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/en/login');
+        $this->assertSelectorTextContains('.card-header', 'Secure Sign in');
+
+        $form = $crawler->selectButton('Sign in')->form([
+            'login_form[username]' => 'user',
+            'login_form[password]' => 'user_admin',
+        ]);
+        $client->submit($form);
+        $client->waitFor('.alert-danger');
+        $this->assertSelectorTextContains('.alert-danger', 'Invalid credentials');
+    }
+
     public function testLoginAsUser(): void
     {
         $client = static::createPantherClient();
@@ -22,5 +37,28 @@ final class AuthTest extends PantherTestCase
         $crawler = $client->waitFor('.js-counter');
         $this->assertSelectorTextContains('h3', 'My properties');
         $this->assertCount(2, $crawler->filter('.card-title'));
+
+        // Log Out
+        $client->request('GET', '/en/logout');
+        $this->assertSelectorTextContains('.h3', 'Popular Listing');
+    }
+
+    public function testLoginAsAdmin(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/en/login');
+        $this->assertSelectorTextContains('.card-header', 'Secure Sign in');
+
+        $form = $crawler->selectButton('Sign in')->form([
+            'login_form[username]' => 'admin',
+            'login_form[password]' => 'admin',
+        ]);
+        $client->submit($form);
+        $client->waitFor('.fa-users-cog');
+        $this->assertSelectorTextContains('#userDropdown', 'Admin');
+
+        // Log Out
+        $client->request('GET', '/en/logout');
+        $this->assertSelectorTextContains('.h3', 'Popular Listing');
     }
 }
