e301723985b2dc7b48570bfe44b1cc7dadc6d55f
Valery Maslov <vmaslov@live.ru>
2022-12-25 11:17:40 +0300

    Update rector config

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SensiolabsSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
@@ -12,5 +13,6 @@ return function (RectorConfig $rectorConfig): void {
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
         SensiolabsSetList::FRAMEWORK_EXTRA_61,
+        LevelSetList::UP_TO_PHP_81,
     ]);
 };
