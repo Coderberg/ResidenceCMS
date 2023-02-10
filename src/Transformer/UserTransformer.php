e5a9656327f34cee5aeefd117946a6c15638c191
Valery Maslov <vmaslov@live.ru>
2023-02-10 11:57:46 +0300

    Refactor the code of transformers

--- src/Transformer/UserTransformer.php
+++ src/Transformer/UserTransformer.php
@@ -9,7 +9,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class UserTransformer
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
     {
     }
 
