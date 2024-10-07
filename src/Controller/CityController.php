e3ba9193e8d944ca683fd0ba83915d8589b4104d
Valery Maslov <valery.maslov@outlook.com>
2024-10-07 12:01:52 +0300

    Fix deprecation warning

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\City;
 use App\Service\CityService;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +14,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CityController extends BaseController
 {
     #[Route(path: '/city/{slug}', name: 'city', defaults: ['page' => 1], methods: ['GET'])]
-    public function index(Request $request, City $city, CityService $service): Response
+    public function index(
+        Request $request,
+        #[MapEntity(mapping: ['slug' => 'slug'])] City $city,
+        CityService $service): Response
     {
         $searchParams = $service->getSearchParams($request, $city);
         $properties = $service->getProperties($searchParams);
